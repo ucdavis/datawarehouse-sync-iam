7af19c01a2ef3fde67e4cfb459a483efc4f00b3f
ltwheeler <ltwheeler@ucdavis.edu>
2019-09-30 21:29:45 -0700

    Update IamPpsDepartmentsImport.java

--- src/main/java/edu/ucdavis/dss/datawarehouse/sync/iam/IamPpsDepartmentsImport.java
+++ src/main/java/edu/ucdavis/dss/datawarehouse/sync/iam/IamPpsDepartmentsImport.java
@@ -7,6 +7,8 @@ import edu.ucdavis.dss.iam.dtos.IamPpsDepartment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
