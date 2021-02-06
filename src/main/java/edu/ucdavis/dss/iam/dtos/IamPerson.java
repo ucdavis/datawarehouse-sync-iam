7637ffae6389cdd0a7680e9b3142ea2abf6f11aa
Jarold Wong <cwjwong@ucdavis.edu>
2021-02-05 16:12:36 -0800

    check for affiliation in IamPerson comparison

--- src/main/java/edu/ucdavis/dss/iam/dtos/IamPerson.java
+++ src/main/java/edu/ucdavis/dss/iam/dtos/IamPerson.java
@@ -41,76 +41,76 @@ public class IamPerson {
 	public void setIamId(Long iamId) {
 		this.iamId = iamId;
 	}
-	
+
 	@Column
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	public Date getModifyDate() {
 		return modifyDate;
 	}
-	
+
 	public void setModifyDate(Date modifyDate) {
 		this.modifyDate = modifyDate;
 	}
-	
+
 	@Column
 	public String getMothraId() {
 		return mothraId;
 	}
-	
+
 	public void setMothraId(String mothraId) {
 		this.mothraId = mothraId;
 	}
-	
+
 	@Column
 	public String getPpsId() {
 		return ppsId;
 	}
-	
+
 	public void setPpsId(String ppsId) {
 		this.ppsId = ppsId;
 	}
-	
+
 	@Column
 	public String getStudentId() {
 		return studentId;
 	}
-	
+
 	public void setStudentId(String studentId) {
 		this.studentId = studentId;
 	}
-	
+
 	@Column
 	public String getBannerPIdM() {
 		return bannerPIdM;
 	}
-	
+
 	public void setBannerPIdM(String bannerPIdM) {
 		this.bannerPIdM = bannerPIdM;
 	}
-	
+
 	@Column
 	public String getExternalId() {
 		return externalId;
 	}
-	
+
 	public void setExternalId(String externalId) {
 		this.externalId = externalId;
 	}
-	
+
 	@Column
 	public String getoFirstName() {
 		return oFirstName;
 	}
-	
+
 	public void setoFirstName(String oFirstName) {
 		this.oFirstName = oFirstName;
 	}
-	
+
 	@Column
 	public String getoMiddleName() {
 		return oMiddleName;
 	}
-	
+
 	public void setoMiddleName(String oMiddleName) {
 		this.oMiddleName = oMiddleName;
 	}
@@ -119,7 +119,7 @@ public class IamPerson {
 	public String getoLastName() {
 		return oLastName;
 	}
-	
+
 	public void setoLastName(String oLastName) {
 		this.oLastName = oLastName;
 	}
@@ -128,105 +128,105 @@ public class IamPerson {
 	public String getoFullName() {
 		return oFullName;
 	}
-	
+
 	public void setoFullName(String oFullName) {
 		this.oFullName = oFullName;
 	}
-	
+
 	@Column
 	public String getoSuffix() {
 		return oSuffix;
 	}
-	
+
 	public void setoSuffix(String oSuffix) {
 		this.oSuffix = oSuffix;
 	}
-	
+
 	@Column
 	public String getdFirstName() {
 		return dFirstName;
 	}
-	
+
 	public void setdFirstName(String dFirstName) {
 		this.dFirstName = dFirstName;
 	}
-	
+
 	@Column
 	public String getdMiddleName() {
 		return dMiddleName;
 	}
-	
+
 	public void setdMiddleName(String dMiddleName) {
 		this.dMiddleName = dMiddleName;
 	}
-	
+
 	@Column
 	public String getdLastName() {
 		return dLastName;
 	}
-	
+
 	public void setdLastName(String dLastName) {
 		this.dLastName = dLastName;
 	}
-	
+
 	@Column
 	public String getdSuffix() {
 		return dSuffix;
 	}
-	
+
 	public void setdSuffix(String dSuffix) {
 		this.dSuffix = dSuffix;
 	}
-	
+
 	@Column
 	public String getdFullName() {
 		return dFullName;
 	}
-	
+
 	public void setdFullName(String dFullName) {
 		this.dFullName = dFullName;
 	}
-	
+
 	@Column(name="isEmployee")
 	@JsonProperty("isEmployee")
 	public Boolean getEmployeeInd() {
 		return employeeInd;
 	}
-	
+
 	public void setEmployeeInd(Boolean employeeInd) {
 		this.employeeInd = employeeInd;
 	}
-	
+
 	@Column(name="isHSEmployee")
 	@JsonProperty("isHSEmployee")
 	public Boolean getHsEmployeeInd() {
 		return hsEmployeeInd;
 	}
-	
+
 	public void sethsEmployeeInd(Boolean hsEmployeeInd) {
 		this.hsEmployeeInd = hsEmployeeInd;
 	}
-	
+
 	@Column(name="isFaculty")
 	@JsonProperty("isFaculty")
 	public Boolean getFacultyInd() {
 		return facultyInd;
 	}
-	
+
 	public void setFacultyInd(Boolean facultyInd) {
 		this.facultyInd = facultyInd;
 	}
-	
+
 	@Column(name="isStudent")
 	@JsonProperty("isStudent")
 	public Boolean getStudentInd() {
 		return studentInd;
 	}
-	
+
 	public void setStudentInd(Boolean studentInd) {
 		this.studentInd = studentInd;
 	}
-	
+
 	@Column(name="isStaff")
 	@JsonProperty("isStaff")
 	public Boolean getStaffInd() {
@@ -235,7 +235,7 @@ public class IamPerson {
 	public void setStaffInd(Boolean staffInd) {
 		this.staffInd = staffInd;
 	}
-	
+
 	@Column(name="isExternal")
 	@JsonProperty("isExternal")
 	public Boolean getExternalInd() {
@@ -244,7 +244,7 @@ public class IamPerson {
 	public void setExternalInd(Boolean externalInd) {
 		this.externalInd = externalInd;
 	}
-	
+
 	@Column
 	public String getPrivacyCode() {
 		return privacyCode;
@@ -337,6 +337,11 @@ public class IamPerson {
 			return false;
 		}
 
-		return true;
+		return employeeInd.equals(other.employeeInd) &&
+			hsEmployeeInd.equals(other.hsEmployeeInd) &&
+			facultyInd.equals(other.facultyInd) &&
+			studentInd.equals(other.studentInd) &&
+			staffInd.equals(other.staffInd) &&
+			externalInd.equals(other.externalInd);
 	}
 }
