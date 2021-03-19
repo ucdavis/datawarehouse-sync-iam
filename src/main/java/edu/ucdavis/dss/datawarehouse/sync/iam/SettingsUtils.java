4ded00ed3b31db036dfb2937aa3cabf6001630bc
Jarold Wong <cwjwong@ucdavis.edu>
2021-03-19 13:22:22 -0700

    remove unused variables

--- src/main/java/edu/ucdavis/dss/datawarehouse/sync/iam/SettingsUtils.java
+++ src/main/java/edu/ucdavis/dss/datawarehouse/sync/iam/SettingsUtils.java
@@ -1,19 +1,13 @@
 package edu.ucdavis.dss.datawarehouse.sync.iam;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Map;
-import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SettingsUtils {
 	private static Logger logger = LoggerFactory.getLogger("SettingsUtils");
-	private static String iamApiKey, ldapUrl, ldapBase, ldapUser, ldapPassword;
+	private static String iamApiKey;
 	private static String elasticSearchHost;
 
 	public static boolean initialize() {
@@ -24,13 +18,9 @@ public class SettingsUtils {
 			Map<String, String> env = System.getenv();
 
 			iamApiKey = env.get("IAM_API_KEY");
-			ldapUrl = env.get("LDAP_URL");
-			ldapBase = env.get("LDAP_BASE");
-			ldapUser = env.get("LDAP_USER");
-			ldapPassword = env.get("LDAP_PASSWORD");
 			elasticSearchHost = env.get("ELASTICSEARCH_HOST");
 			
-			if (iamApiKey != null && ldapUrl != null && ldapBase != null && ldapUser != null && ldapPassword != null && elasticSearchHost != null) {
+			if (iamApiKey != null && elasticSearchHost != null) {
 				return true;
 			} else {
 				System.out.println("Missing an environment variable.");
@@ -40,22 +30,6 @@ public class SettingsUtils {
 			logger.warn("A SecurityException occured while reading environment.");
 			return false;
 		}
-	}
-
-	public static String getLdapUrl() {
-		return ldapUrl;
-	}
-
-	public static String getLdapBase() {
-		return ldapBase;
-	}
-
-	public static String getLdapUser() {
-		return ldapUser;
-	}
-
-	public static String getLdapPassword() {
-		return ldapPassword;
 	}
 
 	public static String getIamApiKey() {
