d789a39b21857e0372037ffeecf0bb70b9ec74d4
Jarold Wong <cwjwong@ucdavis.edu>
2021-07-02 10:00:41 -0700

    match prod schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -6,43 +6,43 @@ DROP TABLE IF EXISTS `iam_pps_associations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `iam_pps_associations` (
-  `deptCode` varchar(10) NOT NULL,
-  `deptOfficialName` varchar(64) NOT NULL,
-  `deptDisplayName` varchar(64) NULL,
-  `deptAbbrev` varchar(24) NOT NULL,
-  `isUCDHS` tinyint(1) NOT NULL,
-  `bouOrgOId` varchar(32) NULL,
-  `assocRank` varchar(2) NOT NULL,
-  `assocStartDate` datetime NOT NULL,
-  `assocEndDate` datetime NULL,
-  `titleCode` varchar(8) NOT NULL,
-  `positionTypeCode` varchar(1) NOT NULL,
-  `createDate` datetime NOT NULL,
-  `modifyDate` datetime NULL,
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `iamId` bigint(20) NOT NULL,
-  `percentFullTime` varchar(6) NOT NULL,
+  `deptCode` varchar(10) NOT NULL,
+  `deptOfficialName` varchar(64) NOT NULL,
+  `deptDisplayName` varchar(64) DEFAULT NULL,
+  `deptAbbrev` varchar(200) NOT NULL DEFAULT '',
+  `isUCDHS` tinyint(1) NOT NULL,
+  `bouOrgOId` varchar(32) DEFAULT NULL,
+  `assocRank` varchar(2) NOT NULL DEFAULT '',
+  `assocStartDate` datetime NOT NULL,
+  `assocEndDate` datetime DEFAULT NULL,
+  `titleCode` varchar(8) NOT NULL,
+  `titleOfficialName` varchar(160) DEFAULT NULL,
   `titleDisplayName` varchar(128) NOT NULL,
+  `positionTypeCode` varchar(1) NOT NULL,
   `positionType` varchar(32) NOT NULL,
-  `titleOfficialName` varchar(160) NULL,
-  `adminBouOrgOId` varchar(32) NULL,
-  `adminDeptAbbrev` varchar(24) NOT NULL,
+  `percentFullTime` varchar(6) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `modifyDate` datetime DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
   `adminDeptCode` varchar(10) NOT NULL,
-  `adminDeptDisplayName` varchar(64) NULL,
-  `adminDeptOfficialName` varchar(64) NOT NULL,
-  `apptBouOrgOId` varchar(32) NULL,
-  `apptDeptAbbrev` varchar(24) NOT NULL,
   `apptDeptCode` varchar(10) NOT NULL,
-  `apptDeptDisplayName` varchar(64) NULL,
+  `adminDeptOfficialName` varchar(64) NOT NULL,
+  `adminDeptDisplayName` varchar(64) DEFAULT NULL,
+  `adminDeptAbbrev` varchar(24) NOT NULL,
   `apptDeptOfficialName` varchar(64) NOT NULL,
-  `createdAt` timestamp NULL,
-  `updatedAt` timestamp NULL,
-  `lastSeen` timestamp NULL,
-  `emplClass` varchar(2) NULL,
-  -- emplClassDesc longest seen was 29 as of 12/19/19, setting higher for safety
-  `emplClassDesc` varchar(64) NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `apptDeptDisplayName` varchar(64) DEFAULT NULL,
+  `apptDeptAbbrev` varchar(24) NOT NULL,
+  `lastSeen` timestamp NULL DEFAULT NULL,
+  `adminBouOrgOId` varchar(32) DEFAULT NULL,
+  `apptBouOrgOId` varchar(32) DEFAULT NULL,
+  `emplClass` varchar(2) DEFAULT NULL,
+  `emplClassDesc` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_iamid` (`iamId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -55,25 +55,26 @@ DROP TABLE IF EXISTS `iam_sis_associations`;
 CREATE TABLE `iam_sis_associations` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `iamId` bigint(20) NOT NULL,
-  `levelCode` varchar(2) NULL,
-  `levelName` varchar(32) NULL,
-  `classCode` varchar(2) NULL,
-  `className` varchar(32) NULL,
-  `collegeCode` varchar(2) NULL,
-  `collegeName` varchar(32) NULL,
-  `assocRank` varchar(2) NULL,
-  `assocStartDate` datetime NULL,
-  `assocEndDate` datetime NULL,
-  `majorCode` varchar(4) NULL,
-  `majorName` varchar(32) NULL,
-  `fepraCode` varchar(1) NULL,
+  `levelCode` varchar(2) DEFAULT NULL,
+  `levelName` varchar(32) DEFAULT NULL,
+  `classCode` varchar(2) DEFAULT NULL,
+  `className` varchar(32) DEFAULT NULL,
+  `collegeCode` varchar(2) DEFAULT NULL,
+  `collegeName` varchar(32) DEFAULT NULL,
+  `assocRank` varchar(2) DEFAULT NULL,
+  `assocStartDate` datetime DEFAULT NULL,
+  `assocEndDate` datetime DEFAULT NULL,
+  `majorCode` varchar(4) DEFAULT NULL,
+  `majorName` varchar(32) DEFAULT NULL,
+  `fepraCode` varchar(1) DEFAULT NULL,
   `createDate` datetime NOT NULL,
-  `modifyDate` datetime NULL,
-  `createdAt` timestamp NULL,
-  `updatedAt` timestamp NULL,
-  `lastSeen` timestamp NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `modifyDate` datetime DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `lastSeen` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_iamid` (`iamId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,25 +85,26 @@ DROP TABLE IF EXISTS `iam_contactinfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `iam_contactinfo` (
+  `iamId` bigint(20) NOT NULL,
+  `email` varchar(48) DEFAULT NULL,
   `hsEmail` varchar(32) DEFAULT NULL,
   `campusEmail` varchar(32) DEFAULT NULL,
-  `workCell` varchar(16) DEFAULT NULL,
-  `workPager` varchar(16) DEFAULT NULL,
-  `workFax` varchar(16) DEFAULT NULL,
-  `iamId` bigint(20) NOT NULL,
-  `workPhone` varchar(32) DEFAULT NULL,
-  `email` varchar(48) DEFAULT NULL,
+  `addrStreet` varchar(128) DEFAULT NULL,
   `addrCity` varchar(32) DEFAULT NULL,
   `addrState` varchar(2) DEFAULT NULL,
   `addrZip` varchar(10) DEFAULT NULL,
   `postalAddress` varchar(96) DEFAULT NULL,
-  `addrStreet` varchar(128) DEFAULT NULL,
+  `workPhone` varchar(32) DEFAULT NULL,
+  `workCell` varchar(16) DEFAULT NULL,
+  `workPager` varchar(16) DEFAULT NULL,
+  `workFax` varchar(16) DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `lastSeen` timestamp NULL DEFAULT NULL,
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `createdAt` timestamp NULL,
-  `updatedAt` timestamp NULL,
-  `lastSeen` timestamp NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `idx_iamid` (`iamId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -113,13 +115,22 @@ DROP TABLE IF EXISTS `iam_people`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `iam_people` (
+  `iamId` bigint(20) NOT NULL,
+  `mothraId` varchar(8) DEFAULT NULL,
+  `ppsId` varchar(9) DEFAULT NULL,
   `studentId` varchar(32) DEFAULT NULL,
   `bannerPIdM` varchar(32) DEFAULT NULL,
   `externalId` varchar(32) DEFAULT NULL,
-  `oFirstName` varchar(64) NULL,
+  `oFirstName` varchar(64) DEFAULT NULL,
+  `oMiddleName` varchar(32) DEFAULT NULL,
   `oLastName` varchar(64) NOT NULL,
   `oFullName` varchar(128) NOT NULL,
   `oSuffix` varchar(20) DEFAULT NULL,
+  `dFirstName` varchar(64) DEFAULT NULL,
+  `dMiddleName` varchar(32) DEFAULT NULL,
+  `dLastName` varchar(64) DEFAULT NULL,
+  `dSuffix` varchar(20) DEFAULT NULL,
+  `dFullName` varchar(128) DEFAULT NULL,
   `isEmployee` tinyint(1) NOT NULL,
   `isHSEmployee` tinyint(1) NOT NULL,
   `isFaculty` tinyint(1) NOT NULL,
@@ -128,23 +139,13 @@ CREATE TABLE `iam_people` (
   `isExternal` tinyint(1) NOT NULL,
   `privacyCode` varchar(16) DEFAULT NULL,
   `modifyDate` datetime NOT NULL,
-  `iamId` bigint(20) NOT NULL,
-  `oMiddleName` varchar(32) DEFAULT NULL,
-  `dFirstName` varchar(32) DEFAULT NULL,
-  `dMiddleName` varchar(32) DEFAULT NULL,
-  `dLastName` varchar(32) DEFAULT NULL,
-  `dSuffix` varchar(20) DEFAULT NULL,
-  `dFullName` varchar(64) DEFAULT NULL,
-  `ppsId` varchar(9) DEFAULT NULL,
-  `mothraId` varchar(8) DEFAULT NULL,
-  `createdAt` timestamp NULL,
-  `updatedAt` timestamp NULL,
-  `lastSeen` timestamp NULL,
+  `UpdatedAt` timestamp NULL DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT NULL,
+  `LastSeen` timestamp NULL DEFAULT NULL,
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
-  -- FULLTEXT KEY `search_key` (`oFirstName`, `oMiddleName`, `oLastName`, `oFullName`, `dFirstName`, `dMiddleName`, `dLastName`, `dFullName`)
-  FULLTEXT KEY `search_key` (`oFullName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `idx_iamid` (`iamId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -155,20 +156,20 @@ DROP TABLE IF EXISTS `iam_pps_depts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `iam_pps_depts` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `orgOId` varchar(32) NOT NULL,
   `deptCode` varchar(10) NOT NULL,
   `deptOfficialName` varchar(64) NOT NULL,
-  `deptDisplayName` varchar(64) NULL,
-  `deptAbbrev` varchar(24) NOT NULL,
+  `deptDisplayName` varchar(64) DEFAULT NULL,
+  `deptAbbrev` varchar(200) NOT NULL DEFAULT '',
   `isUCDHS` tinyint(1) NOT NULL,
-  `bouOrgOId` varchar(32) NULL,
+  `bouOrgOId` varchar(32) DEFAULT NULL,
   `createDate` datetime NOT NULL,
   `modifyDate` datetime NOT NULL,
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `createdAt` timestamp NULL,
-  `updatedAt` timestamp NULL,
+  `UpdatedAt` timestamp NULL DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -179,20 +180,20 @@ DROP TABLE IF EXISTS `iam_prikerbacct`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `iam_prikerbacct` (
+  `iamId` bigint(20) NOT NULL,
   `userId` varchar(32) NOT NULL,
-  `uuId` varchar(32) NOT NULL,
+  `uuId` varchar(32) DEFAULT NULL,
   `createDate` datetime NOT NULL,
   `claimDate` datetime DEFAULT NULL,
   `expireDate` datetime DEFAULT NULL,
+  `UpdatedAt` timestamp NULL DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT NULL,
+  `LastSeen` timestamp NULL DEFAULT NULL,
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `iamId` bigint(20) NOT NULL,
-  `createdAt` timestamp NULL,
-  `updatedAt` timestamp NULL,
-  `lastSeen` timestamp NULL,
   PRIMARY KEY (`id`),
-  KEY `idx_iamid` (`iamId`),
-  INDEX `iam_p_iam` (`iamId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  KEY `idx_userid` (`userId`),
+  KEY `idx_iamid` (`iamId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -206,15 +207,15 @@ CREATE TABLE `iam_bous` (
   `orgOId` varchar(32) NOT NULL,
   `deptCode` varchar(6) NOT NULL,
   `deptOfficialName` varchar(64) NOT NULL,
-  `deptDisplayName` varchar(32) NOT NULL,
+  `deptDisplayName` varchar(32) DEFAULT '',
   `deptAbbrev` varchar(32) NOT NULL,
-  `isUCDHS` boolean NOT NULL,
+  `isUCDHS` tinyint(1) NOT NULL,
   `createDate` datetime NOT NULL,
-  `modifyDate` datetime NULL,
-  `createdAt` timestamp NULL,
-  `updatedAt` timestamp NULL,
+  `modifyDate` datetime DEFAULT NULL,
+  `UpdatedAt` timestamp NULL DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`orgOId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
